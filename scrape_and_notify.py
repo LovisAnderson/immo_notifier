7deb1ff8972ff2c169489d56348fc8163b55dff0
lovis <anderson@zib.de>
2020-07-11 18:37:08 +0200

    fix: message sending using html
    - did not work with web.de and other services

--- scrape_and_notify.py
+++ scrape_and_notify.py
@@ -4,6 +4,8 @@ import json
 import smtplib, ssl
 from config import *
 from pathlib import Path
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # Create a secure SSL context
 context = ssl.create_default_context()
@@ -35,7 +37,8 @@ def find_new_listings(ids, listings, search_identifier):
     return new_listings
 
 
-for config in (lovis_cfg, shana_cfg):
+for config in (lovis_cfg,
+               shana_cfg):
     sender_email = config['sender_email']
     receiver_email = config['receiver_email']
     password = config['password']
@@ -45,25 +48,30 @@ for config in (lovis_cfg, shana_cfg):
         new_listings = find_new_listings(ids, listings, search_name)
         if len(new_listings) == 0:
             continue
-        listings[search_name] ={**listings[search_name], **new_listings}
-        subject = ""
-        mail_text = ""
-        subject += f'{len(new_listings.keys())} neue {search_name}! '
-        mail_text += '<p>Wie waers damit? </p>'
-        mail_text += "".join([f'<a href=\"{link}\">Wohnung {id}</a><br />\n' for id, link in new_listings.items()])
+        listings[search_name] = {**listings[search_name], **new_listings}
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = f'{len(new_listings.keys())} neue {search_name}! '
+        msg['From'] = sender_email
+        msg['To'] = receiver_email
 
-        message = f"""From: Me <{sender_email}>
-To: Myself <{receiver_email}>
-MIME-Version: 1.0
-Content-type: text/html
-Subject: {subject}
-    
-{mail_text}
-"""
+        mail_html = '<p>Wie waers damit? </p>'
+        mail_html += "".join([f'<a href=\"{link}\">Wohnung {id}</a><br />\n' for id, link in new_listings.items()])
+
+        mail_text = 'Wie waers damit?\n'
+        mail_text = "".join([f'{link}>\n' for id, link in new_listings.items()])
+        # Record the MIME types of both parts - text/plain and text/html.
+        part1 = MIMEText(mail_text, 'plain')
+        part2 = MIMEText(mail_html, 'html')
+
+        # Attach parts into message container.
+        # According to RFC 2046, the last part of a multipart message, in this case
+        # the HTML message, is best and preferred.
+        msg.attach(part1)
+        msg.attach(part2)
 
         with open(listings_file, 'w') as file:
             json.dump(listings, file)
 
         with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
             server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
+            server.sendmail(sender_email, receiver_email, msg.as_string())
