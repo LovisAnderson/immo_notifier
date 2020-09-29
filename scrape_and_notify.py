6e3011e1fa320d018ef387fbd72c3f2b27414087
Lovis <lovis_anderson@hotmail.com>
2020-09-29 22:29:30 +0200

    ref/add: moved scraping classes to own file and added scrapers

--- scrape_and_notify.py
+++ scrape_and_notify.py
@@ -1,86 +1,9 @@
-import urllib.request
-import re
-import json
 import smtplib, ssl
 from config import *
-from pathlib import Path
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from contextlib import closing
 
-from selenium.webdriver import Firefox
-from abc import ABC, abstractmethod
-
-
-class Scraper(ABC):
-    def __init__(self, url, identifier, engine='selenium', listings_file='listings.json'):
-        self.url = url
-        self.identifier = identifier
-        self.engine = engine
-        self._html = None
-        self.listings_file = Path(__file__).parent /  listings_file
-        self._listings = None
-
-    @property
-    def html(self):
-        if self._html is None:
-            self._html = self.get_html()
-        return self._html
-
-    @property
-    def listings(self):
-        if self._listings is None:
-            with open(self.listings_file) as file:
-                all_listings = json.load(file)
-                listings = all_listings.get(self.identifier, {})
-            self._listings = listings
-        return self._listings
-
-    @listings.setter
-    def listings(self, listings):
-        self._listings = {**self.listings, **listings}
-
-    def get_html(self):
-        if self.engine == 'selenium':
-            with closing(Firefox()) as browser:
-                browser.get(self.url)
-                html = browser.page_source
-        elif self.engine == 'urllib':
-            search_url = urllib.request.urlopen(self.url)
-            html = search_url.read()
-
-            html = html.decode("utf8")
-            search_url.close()
-        return html
-
-    @abstractmethod
-    def listings_from_html(self):
-        pass
-
-    def new_listings(self):
-        new_listings = {}
-        for listing_id, url in self.listings_from_html().items():
-            if listing_id not in self.listings:
-                new_listings[listing_id] = url
-        return new_listings
-
-    def save_listings(self):
-        with open(self.listings_file, 'r') as file:
-            all_listings = json.load(file)
-        with open(self.listings_file, 'w') as file:
-            all_listings[self.identifier] = self.listings
-            json.dump(all_listings, file, indent=4, sort_keys=True)
-
-
-class ImmoScout(Scraper):
-    def __init__(self, url, identifier, engine='selenium', listings_file='listings.json'):
-        super(ImmoScout, self).__init__(url, identifier, engine, listings_file)
-
-    def listings_from_html(self):
-        pattern = r'data-go-to-expose-id=\"[0-9]+\"'  # data-go-to-expose-id="119962085"
-        matches = re.findall(pattern, self.html)
-        ids = [match.split("\"")[1] for match in matches]
-        return {ID: f"https://www.immobilienscout24.de/expose/{ID}" for ID in ids}
+from scraper import get_scraper
 
 
 class Mailer:
@@ -136,7 +59,7 @@ for config in (lovis_cfg,
     password = config['password']
 
     for search_name, url in config['searches'].items():
-        scraper = ImmoScout(url, search_name)
+        scraper = get_scraper(url)(url, search_name)
         new_listings = scraper.new_listings()
         if len(new_listings) == 0:
             continue
